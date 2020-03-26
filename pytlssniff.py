3a70222367887a2a85a4443407a4ff3e2b53b08f
M4t7e <7535523+M4t7e@users.noreply.github.com>
2020-03-26 14:40:12 +0100

    Added file capture mode

--- pytlssniff.py
+++ pytlssniff.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
-import pyshark
 import binascii
 import base64
 import OpenSSL.crypto
 from typing import NamedTuple, Iterator, Optional, List
 from enum import Enum
+from pyshark import FileCapture, LiveCapture
+from pyshark.packet.packet import Packet
 
 
 class TLSHandshakeType(Enum):
@@ -46,8 +47,9 @@ class TLSHandshakeMessage(NamedTuple):
 
 
 class TLSHandshakeSniffer():
-    def __init__(self, interface='any', custom_bpf_filter='', custom_display_filter=''):
+    def __init__(self, interface='any', input_file=None, custom_bpf_filter='', custom_display_filter=''):
         self.interface = interface
+        self.input_file = input_file
         self.custom_bpf_filter = custom_bpf_filter
         self.custom_display_filter = custom_display_filter
 
@@ -84,6 +86,55 @@ class TLSHandshakeSniffer():
         except Exception:
             return None
 
+    @classmethod
+    def _parse_packet(self, packet: Packet, sniff_sni=True, sniff_cn=True, sniff_san=True) -> Optional[TLSHandshakeMessage]:
+        san = None
+        cn = None
+        sni = None
+
+        try:
+            handshake_type = TLSHandshakeType(int(packet.ssl.handshake_type))
+
+            if sniff_sni and 'handshake_extensions_server_name' in packet.ssl.field_names and packet.ssl.handshake_extensions_server_name != '':
+                sni = packet.ssl.handshake_extensions_server_name.lower()
+
+            if sniff_cn and 'x509ce_dnsname' in packet.ssl.field_names:
+                cn = packet.ssl.x509ce_dnsname.lower()
+
+            # Hopefully the SAN section will also be accessible with pyshark in future
+            if sniff_san and 'handshake_certificate' in packet.ssl.field_names:
+                cert = self._parse_certificate(packet.ssl.handshake_certificate)
+                if cert is not None:
+                    san = self._extract_certificate_san(cert)
+
+            if 'ip' in packet:
+                ip_version = 4
+                src_ip = packet.ip.src
+                dst_ip = packet.ip.dst
+            elif 'ipv6' in packet:
+                ip_version = 6
+                src_ip = packet.ipv6.src
+                dst_ip = packet.ipv6.dst
+            else:
+                return None
+
+            if sni is not None or cn is not None or san is not None:
+                return TLSHandshakeMessage(
+                    handshake_type=handshake_type,
+                    ip_version=ip_version,
+                    src_ip=src_ip,
+                    src_port=int(packet.tcp.srcport),
+                    dst_ip=dst_ip,
+                    dst_port=int(packet.tcp.dstport),
+                    sni=sni,
+                    cn=cn,
+                    san=san
+                )
+
+            return None
+        except Exception:
+            return None
+
     def listen(self, sniff_sni=True, sniff_cn=True, sniff_san=True, packet_count: int = None, debug: bool = False) -> Iterator[TLSHandshakeMessage]:
         # Currently only IPv4 is supported for BPF tcp data access. Manpage says: "this will be fixed in the future" for IPv6.
         # Until then, only the 'tcp' filter is applied
@@ -99,57 +150,22 @@ class TLSHandshakeSniffer():
         if packet_count is not None and packet_count <= 0:
             packet_count = None
 
-        capture = pyshark.LiveCapture(interface=self.interface, bpf_filter=bpf_filter, display_filter=display_filter, debug=debug)
-        for packet in capture.sniff_continuously():
-            san = None
-            cn = None
-            sni = None
+        if self.input_file is not None:
+            packet_iterator = iter(FileCapture(input_file=self.input_file, display_filter=display_filter, debug=debug))
+        else:
+            capture = LiveCapture(interface=self.interface, bpf_filter=bpf_filter, display_filter=display_filter, debug=debug)
+            packet_iterator = capture.sniff_continuously()
 
-            try:
-                handshake_type = TLSHandshakeType(int(packet.ssl.handshake_type))
+        for packet in packet_iterator:
+            handshake_message = self._parse_packet(packet, sniff_sni=sniff_sni, sniff_cn=sniff_cn, sniff_san=sniff_san)
 
-                if sniff_sni and 'handshake_extensions_server_name' in packet.ssl.field_names and packet.ssl.handshake_extensions_server_name != '':
-                    sni = packet.ssl.handshake_extensions_server_name.lower()
+            if handshake_message is not None:
+                yield handshake_message
 
-                if sniff_cn and 'x509ce_dnsname' in packet.ssl.field_names:
-                    cn = packet.ssl.x509ce_dnsname.lower()
-
-                # Hopefully the SAN section will also be accessible with pyshark in future
-                if sniff_san and 'handshake_certificate' in packet.ssl.field_names:
-                    cert = self._parse_certificate(packet.ssl.handshake_certificate)
-                    if cert is not None:
-                        san = self._extract_certificate_san(cert)
-
-                if 'ip' in packet:
-                    ip_version = 4
-                    src_ip = packet.ip.src
-                    dst_ip = packet.ip.dst
-                elif 'ipv6' in packet:
-                    ip_version = 6
-                    src_ip = packet.ipv6.src
-                    dst_ip = packet.ipv6.dst
-                else:
-                    continue
-                
-                if sni is not None or cn is not None or san is not None:
-                    yield TLSHandshakeMessage(
-                        handshake_type=handshake_type,
-                        ip_version=ip_version,
-                        src_ip=src_ip,
-                        src_port=int(packet.tcp.srcport),
-                        dst_ip=dst_ip,
-                        dst_port=int(packet.tcp.dstport),
-                        sni=sni,
-                        cn=cn,
-                        san=san
-                    )
-
-                    if packet_count is not None:
-                        packet_count -= 1
-                        if packet_count <= 0:
-                            break
-            except Exception:
-                pass
+                if packet_count is not None:
+                    packet_count -= 1
+                    if packet_count <= 0:
+                        break
 
         raise StopIteration
 
@@ -167,10 +183,12 @@ def parse_args():
                         help="sniff Common Name from certificate CN section")
     parser.add_argument("-i", '--interface', dest='interface', default="any",
                         help="name or idx of interface (default: any)", required=False)
+    parser.add_argument("-r", '--input-file', dest='input_file', default=None,
+                        help="set the filename to read from (- to read from stdin)", required=False)
     parser.add_argument("-p", '--packet-count', dest='packet_count', type=int, default=None,
                         help="stop after n packets (def: infinite)", required=False)
     parser.add_argument("-b", "--bpf-filter", dest="bpf_filter", default="",
-                        help="packet filter in Berkeley Packet Filter (BPF) syntax", required=False)
+                        help="packet filter in Berkeley Packet Filter (BPF) syntax (for live trace only)", required=False)
     parser.add_argument("-Y", "--display-filter", dest="display_filter", default="",
                         help="packet displaY filter in Wireshark display filter", required=False)
 
@@ -179,7 +197,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    handshake_sniffer = TLSHandshakeSniffer(args.interface, args.bpf_filter, args.display_filter)
+    handshake_sniffer = TLSHandshakeSniffer(args.interface, args.input_file, args.bpf_filter, args.display_filter)
 
     if not (args.sni or args.cn or args.san):
         args.sni = True
@@ -213,6 +231,7 @@ def main():
                 f"{message.handshake_type.name}({message.handshake_type.value})\t{ip_version}\t"
                 f"{message.src_ip}:{message.src_port}\t{message.dst_ip}:{message.dst_port}\t" + f"{dns_name}", flush=True
             )
+
 
 if __name__ == "__main__":
     main()
