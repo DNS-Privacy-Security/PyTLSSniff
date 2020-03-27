28daa49b82ec0e3841867b1464a9bde7a8a41a51
M4t7e <7535523+M4t7e@users.noreply.github.com>
2020-03-27 20:12:13 +0100

    Created new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as fh:
 
 setuptools.setup(
     name="PyTLSSniff",
-    version="0.0.1",
+    version="0.0.2",
     author="M4t7e",
     license='MIT License',
     description="Python TLS handshake sniffer to extract domain names",
